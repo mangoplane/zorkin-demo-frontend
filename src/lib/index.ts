ec6d3ab412ad9a6c5fa3583928251792ee2b8b82
mangoplane <devmon1@protonmail.com>
2024-10-09 13:14:00 +1100

    refactor: new demo oidc client id

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,17 +1,6 @@
 import Zorkin from 'zorkin-sdk'
 import { googleClientId, DEMO_CLIENT_ID } from '../constants'
 
-function base64ToBase64url(base64: string): string {
-  return base64
-    .replace(/\+/g, '-') // Replace '+' with '-'
-    .replace(/\//g, '_') // Replace '/' with '_'
-    .replace(/=+$/, '') // Remove any trailing '=' padding characters
-}
-
-function bufferToBase64url(buffer: Uint8Array | ArrayBuffer): string {
-  return base64ToBase64url(Buffer.from(buffer).toString('base64'))
-}
-
 export const zorkin = new Zorkin({
   OAuthClients: {
     Google: {
@@ -19,5 +8,5 @@ export const zorkin = new Zorkin({
       ClientId: googleClientId
     }
   },
-  clientID: bufferToBase64url(DEMO_CLIENT_ID),
+  clientID: DEMO_CLIENT_ID
 })
