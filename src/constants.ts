78d2b7b8664464baf86e3bf7547cafaf179fd7ed
mangoplane <devmon1@protonmail.com>
2024-10-09 22:00:19 +1100

    fix: add new client that has an active subscription.

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const googleClientId = "210383238890-6hmgcfd0pmu15t8sic15b9opvrf2mlkb.apps.googleusercontent.com"
-export const DEMO_CLIENT_ID = "X4Mvd3r-Z7sa5tC-6JLUgg30hOv7k1l_jHMZqDMyhUk"
+export const DEMO_CLIENT_ID = "Zo6u4ehXjfGq6m4aSdazFmhR2hRg_kW0RCcu10nnUaY"
